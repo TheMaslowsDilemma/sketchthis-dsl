248f890d9498c3b32e58460affbf18aad0507175
TheMaslowsDilemma <themaslowsdilemma@gmail.com>
2026-01-16 13:19:44 -0500

    prep for sketch-studio

--- bin/main.ml
+++ bin/main.ml
@@ -4,23 +4,67 @@
 open Sketch_dsl
 
 let usage =
-  {|
-Sketch DSL Compiler
+  {|Sketch DSL Compiler
 
-Usage: sketch_dsl [options] [file]
+Usage: sketch_dsl <input-file> [options]
 
 Options:
-  --lex         Tokenize input and print tokens
-  --parse       Parse input and print AST
-  --compile     Compile input and print IR
-  --gcode       Generate generic G-code
-  --machine     Generate G-code for Uunatek plotter (default)
-  --svg         Generate SVG preview
-  --stats       Show compilation and optimization statistics
-  --help        Show this help message
+  -o <name>     Output base name (without extension)
+  --gcode       Generate G-code output (.txt)
+  --svg         Generate SVG preview (.svg)
+  --stats       Print compilation statistics to stdio
 
-If no file is provided, reads from stdin.
+If neither --gcode nor --svg is specified, defaults to --gcode.
+If -o is not specified, uses input filename as base.
+
+Examples:
+  sketch_dsl fun.sketch                     # outputs fun.txt
+  sketch_dsl fun.sketch -o out              # outputs out.txt
+  sketch_dsl fun.sketch -o out --svg        # outputs out.svg only
+  sketch_dsl fun.sketch -o out --gcode --svg  # outputs out.txt and out.svg
 |}
+
+type config = {
+  input_file : string option;
+  output_base : string option;
+  emit_gcode : bool;
+  emit_svg : bool;
+  show_stats : bool;
+}
+
+let default_config = {
+  input_file = None;
+  output_base = None;
+  emit_gcode = false;
+  emit_svg = false;
+  show_stats = false;
+}
+
+let parse_args args =
+  let rec loop cfg = function
+    | [] -> cfg
+    | "-o" :: name :: rest -> loop { cfg with output_base = Some name } rest
+    | "--gcode" :: rest -> loop { cfg with emit_gcode = true } rest
+    | "--svg" :: rest -> loop { cfg with emit_svg = true } rest
+    | "--stats" :: rest -> loop { cfg with show_stats = true } rest
+    | "-h" :: _ | "--help" :: _ -> print_endline usage; exit 0
+    | arg :: rest when String.length arg > 0 && arg.[0] <> '-' ->
+        loop { cfg with input_file = Some arg } rest
+    | arg :: _ ->
+        Printf.eprintf "Unknown option: %s\n%s" arg usage;
+        exit 1
+  in
+  let cfg = loop default_config args in
+  (* Default to gcode if neither specified *)
+  if not cfg.emit_gcode && not cfg.emit_svg then
+    { cfg with emit_gcode = true }
+  else
+    cfg
+
+let remove_extension filename =
+  match String.rindex_opt filename '.' with
+  | Some i -> String.sub filename 0 i
+  | None -> filename
 
 let read_file filename =
   let ic = open_in filename in
@@ -29,203 +73,64 @@ let read_file filename =
   close_in ic;
   s
 
-let read_stdin () =
-  let buf = Buffer.create 1024 in
-  try
-    while true do
-      Buffer.add_string buf (input_line stdin);
-      Buffer.add_char buf '\n'
-    done;
-    ""
-  with End_of_file -> Buffer.contents buf
+let write_file filename content =
+  let oc = open_out filename in
+  output_string oc content;
+  close_out oc
 
-let run_lexer input =
-  try
-    let tokens = Lexer.tokenize input in
-    print_endline "=== Tokens ===";
-    print_endline (Lexer.located_tokens_to_string tokens);
-    print_endline "";
-    print_endline "=== Compact ===";
-    print_endline
-      (Lexer.tokens_to_string (List.map (fun t -> t.Lexer.token) tokens))
-  with Lexer.LexerError err ->
-    Printf.eprintf "Lexer error at line %d, column %d: %s\n" err.position.line
-      err.position.column err.message;
-    exit 1
-
-let run_parser input =
-  try
-    let ast = Parser.parse input in
-    print_endline "=== AST ===";
-    print_endline (Ast.program_to_string ast);
-    print_endline "";
-    print_endline
-      (Printf.sprintf "=== Parsed %d statement(s) ===" (List.length ast))
-  with
-  | Lexer.LexerError err ->
-      Printf.eprintf "Lexer error at line %d, column %d: %s\n" err.position.line
-        err.position.column err.message;
-      exit 1
-  | Parser.ParseError err ->
-      Printf.eprintf "%s\n" (Parser.format_error err);
-      exit 1
-
-let run_compiler ?(show_stats = false) input =
-  try
-    let ast = Parser.parse input in
-    let ir = Compiler.compile ast in
-    print_endline "=== Intermediate Representation ===";
-    print_endline (Compiler.ir_to_string ir);
-    if show_stats then begin
-      print_endline "";
-      print_endline "=== Statistics ===";
-      print_endline (Compiler.ir_stats ir)
-    end
-  with
-  | Lexer.LexerError err ->
-      Printf.eprintf "Lexer error at line %d, column %d: %s\n" err.position.line
-        err.position.column err.message;
-      exit 1
-  | Parser.ParseError err ->
-      Printf.eprintf "%s\n" (Parser.format_error err);
-      exit 1
-  | Compiler.CompileError err ->
-      Printf.eprintf "Compile error: %s\n" (Compiler.format_error err);
-      exit 1
-
-let run_gcode ?(show_stats = false) input =
-  try
-    let ast = Parser.parse input in
-    let ir = Compiler.compile ast in
-    if show_stats then begin
-      let gcode, stats = Gcode.generate_with_stats ir in
-      print_endline gcode;
-      print_endline "";
-      print_endline "=== Optimization Statistics ===";
-      print_endline stats
-    end
-    else begin
-      let gcode = Gcode.generate ir in
-      print_endline gcode
-    end
-  with
-  | Lexer.LexerError err ->
-      Printf.eprintf "Lexer error at line %d, column %d: %s\n" err.position.line
-        err.position.column err.message;
-      exit 1
-  | Parser.ParseError err ->
-      Printf.eprintf "%s\n" (Parser.format_error err);
-      exit 1
-  | Compiler.CompileError err ->
-      Printf.eprintf "Compile error: %s\n" (Compiler.format_error err);
-      exit 1
-
-let run_svg input output_filepath =
-  try
-    let ast = Parser.parse input in
-    let ir = Compiler.compile ast in
-    let svg = Gcode.generate_svg ir in
-    let oc = open_out output_filepath in
-    Printf.fprintf oc "%s" svg;
-    close_out oc
-  with
-  | Lexer.LexerError err ->
-      Printf.eprintf "Lexer error at line %d, column %d: %s\n" err.position.line
-        err.position.column err.message;
-      exit 1
-  | Parser.ParseError err ->
-      Printf.eprintf "%s\n" (Parser.format_error err);
-      exit 1
-  | Compiler.CompileError err ->
-      Printf.eprintf "Compile error: %s\n" (Compiler.format_error err);
-      exit 1
-
-let run_machine ?(show_stats = false) input =
-  try
-    let ast = Parser.parse input in
-    let ir = Compiler.compile ast in
+let compile_input input ~emit_gcode ~emit_svg ~output_base ~show_stats =
+  let ast = Parser.parse input in
+  let ir = Compiler.compile ast in
+  
+  if emit_gcode then begin
+    let gcode_file = output_base ^ ".txt" in
     if show_stats then begin
       let gcode, stats = Gcode.generate_machine_with_stats ir in
-      print_endline gcode;
-      print_endline "";
-      print_endline "=== Uunatek Statistics ===";
-      print_endline stats
-    end
-    else begin
+      write_file gcode_file gcode;
+      Printf.printf "Compilation Stats: %s\n" stats
+    end else begin
       let gcode = Gcode.generate_machine ir in
-      print_endline gcode
+      write_file gcode_file gcode
     end
-  with
-  | Lexer.LexerError err ->
-      Printf.eprintf "Lexer error at line %d, column %d: %s\n" err.position.line
-        err.position.column err.message;
-      exit 1
-  | Parser.ParseError err ->
-      Printf.eprintf "%s\n" (Parser.format_error err);
-      exit 1
-  | Compiler.CompileError err ->
-      Printf.eprintf "Compile error: %s\n" (Compiler.format_error err);
-      exit 1
-  | Failure msg ->
-      Printf.eprintf "Error: %s\n" msg;
-      exit 1
+  end;
+  
+  if emit_svg then begin
+    let svg_file = output_base ^ ".svg" in
+    let svg = Gcode.generate_svg ir in
+    write_file svg_file svg
+  end
 
 let () =
   let args = Array.to_list Sys.argv |> List.tl in
-
-  match args with
-  | [ "--help" ] | [ "-h" ] -> print_endline usage
-  | [ "--lex" ] ->
-      let input = read_stdin () in
-      run_lexer input
-  | [ "--lex"; filename ] ->
-      let input = read_file filename in
-      run_lexer input
-  | [ "--parse" ] ->
-      let input = read_stdin () in
-      run_parser input
-  | [ "--parse"; filename ] ->
-      let input = read_file filename in
-      run_parser input
-  | [ "--compile" ] ->
-      let input = read_stdin () in
-      run_compiler input
-  | [ "--compile"; filename ] ->
-      let input = read_file filename in
-      run_compiler ~show_stats:true input
-  | [ "--gcode" ] ->
-      let input = read_stdin () in
-      run_gcode input
-  | [ "--gcode"; filename ] ->
-      let input = read_file filename in
-      run_gcode input
-  | [ "--svg"; "-o"; output_path ] ->
-      let input = read_stdin () in
-      run_svg input output_path
-  | [ "--svg"; "-i"; filename; "-o"; output_path ] ->
-      let input = read_file filename in
-      run_svg input output_path
-  | [ "--machine" ] ->
-      let input = read_stdin () in
-      run_machine input
-  | [ "--machine"; filename ] ->
-      let input = read_file filename in
-      run_machine input
-  | [ "--stats" ] ->
-      let input = read_stdin () in
-      run_machine ~show_stats:true input
-  | [ "--stats"; filename ] ->
-      let input = read_file filename in
-      run_machine ~show_stats:true input
-  | [ filename ] ->
-      (* Default: generate Uunatek G-code with stats *)
-      let input = read_file filename in
-      run_machine ~show_stats:true input
-  | [] ->
-      (* Interactive mode - read from stdin *)
-      print_endline "Sketch DSL - Enter your program (Ctrl+D to finish):";
-      let input = read_stdin () in
-      run_machine ~show_stats:true input
-  | _ ->
-      prerr_endline usage;
+  let cfg = parse_args args in
+  
+  match cfg.input_file with
+  | None ->
+      Printf.eprintf "Error: No input file specified\n%s" usage;
       exit 1
+  | Some input_file ->
+      let output_base = match cfg.output_base with
+        | Some base -> base
+        | None -> remove_extension input_file
+      in
+      try
+        let input = read_file input_file in
+        compile_input input
+          ~emit_gcode:cfg.emit_gcode
+          ~emit_svg:cfg.emit_svg
+          ~output_base
+          ~show_stats:cfg.show_stats
+      with
+      | Lexer.LexerError err ->
+          Printf.eprintf "Lexer error at line %d, column %d: %s\n"
+            err.position.line err.position.column err.message;
+          exit 1
+      | Parser.ParseError err ->
+          Printf.eprintf "%s\n" (Parser.format_error err);
+          exit 1
+      | Compiler.CompileError err ->
+          Printf.eprintf "Compile error: %s\n" (Compiler.format_error err);
+          exit 1
+      | Sys_error msg ->
+          Printf.eprintf "Error: %s\n" msg;
+          exit 1
