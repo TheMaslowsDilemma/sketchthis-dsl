f0dcb3a491da4c89216086ce4c4ad4520e6ab83d
TheMaslowsDilemma <themaslowsdilemma@gmail.com>
2026-02-20 06:28:32 -0500

    update for sections

--- lib/ir.ml
+++ lib/ir.ml
@@ -1,9 +1,7 @@
-(*
------------------------------------------------------------
-ir.ml - intermediate representation
------------------------------------------------------------
-definitions of ir, segment, and path
-*)
+(*---------------------------------------------------------
+ir.ml - intermediate representation - definitions of ir,
+segment, and path
+---------------------------------------------------------*)
 
 type segment = { p0 : Vector.vec; p1 : Vector.vec }
 type path = { start : Vector.vec; segments : segment list }
--- lib/location.ml
+++ lib/location.ml
@@ -1,10 +1,6 @@
-(*
------------------------------------------------------------
-location.ml
------------------------------------------------------------
-inspired by Ocamls parser
-https://github.com/ocaml/ocaml/blob/trunk/parsing/location.mli
-*)
+(*---------------------------------------------------------
+location.ml - inspired by Ocamls parser parsing/location.mli
+---------------------------------------------------------*)
 
 type t = { start_loc : Lexer.position; end_loc : Lexer.position }
 type 'a loc = { txt : 'a; loc : t }
